7b0ba841d3e460501e61d10451a7bde8554587b9
Zack <jonaenglish64@gmail.com>
2022-10-31 13:56:12 -0600

    Fix nav bar

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@
     <router-link :to="{ name: 'counter' }">Counter</router-link> |
     <router-link :to="{ name: 'users' }">Usuarios</router-link> | 
     <router-link :to="{ name: 'todo' }">Todo</router-link> |
-    <router-link :to="{ name: 'slots' }">Slots</router-link>
-    <router-link :to="{ name: 'users' }">Usuarios</router-link> |
+    <router-link :to="{ name: 'slots' }">Slots</router-link> |
     <router-link :to="{ name: 'pokemon-search' }">Buscar</router-link>
   </nav>
   <router-view v-slot="{ Component, route }">
